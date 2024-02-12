fc9c7ff538ab2247ab122811dcf50c3081bb4e79
Pozok05 <a2308766@institutmontilivi.cat>
2024-02-12 12:17:38 +0100

    Començant metodes cistella

--- BotigaCistella/BotigaCistella/Program.cs
+++ BotigaCistella/BotigaCistella/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            (string[] productes, int[] quantitat, int nElem, double diners) cistella = (new string[5], new int[5], 0, 0);
+            /*
+            cistella.productes[0] = "Durum";
+            cistella.quantitat[0] = 1;
+            cistella.nElem++;
+            cistella.diners += 5.20;
+            */
+        }
+        static void ComprarProducte(ref (string[] productes, int[] quantitat, int nElem, double diners) cistella, ref (string[] producte, double[] preu, int nElem) botiga ,string producte, int quantitat)
+        {
+            if (ExisteixABotiga(botiga, producte))
+            {
+
+            }
+            else Console.WriteLine("ERROR El producte no existeix a la botiga");
+        }
+        static bool ExisteixABotiga((string[] productes, double[] preu, int nElem) botiga, string producte)
+        {
+            int i = 0;
+            while(i < 0 && botiga.productes[i] != producte)
+            {
+                i++;
+            }
+            return botiga.productes[i] == producte;
         }
     }
 }
